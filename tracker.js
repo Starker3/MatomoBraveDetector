6a423bc792bfd1cb03138d202cb4978c7bf1babb
Starker3 <richard@stark-records.com>
2023-01-27 21:09:07 +1300

    Add Try/Catch block

--- tracker.js
+++ tracker.js
@@ -1,6 +1,11 @@
 (function() {
+    try {
     if (window.navigator.brave && window.navigator.brave.isBrave() || false){
         let brave_ua = navigator.userAgent.replace("Chrome","Chrome(Brave)");
         _paq.push(['appendToTrackingUrl', 'ua='+brave_ua]); 
        }
+    }
+    catch (error) {
+        console.error(error);
+    }
 })();
